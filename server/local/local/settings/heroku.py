24e79ef589aa0b2f645d12da76756c6718ceefa9
Davide Callegari <admin@brokenseal.it>
2013-08-30 11:11:09 +0200

    Fixed wrong redis configuration

--- server/local/local/settings/heroku.py
+++ server/local/local/settings/heroku.py
@@ -1,2 +1,12 @@
+import os
+
 CHANNEL_MAX_AGE = 40
 DISABLED_TRANSPORTS = ['websocket']
+
+REDIS_URL = None
+for _redis_url in ('REDISCLOUD_URL', 'REDISTOGO_URL', 'OPENREDIS_URL'):
+    if _redis_url in os.environ:
+        REDIS_URL = os.environ[_redis_url]
+        break
+
+assert REDIS_URL is not None, "Redis not installed"
